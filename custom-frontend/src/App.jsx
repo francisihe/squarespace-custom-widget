52189048335f2b7fe92d8a51242549ffc4ed3268
francisihe <francis.ihejirika@gmail.com>
2024-03-21 03:27:04 +0100

    CODE: Tested retrieval of URL keywords and storing in state

--- custom-frontend/src/App.jsx
+++ custom-frontend/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Set the state from url parameters using useEffect and useState
+  const [keyword, setKeyword] = useState('');
+  const [category, setCategory] = useState('');
+
+  // Get the URL parameters
+  const urlParams = new URLSearchParams(window.location.search);
+  const keywordFromUrl = urlParams.get('keyword');
+  const categoryFromUrl = urlParams.get('category');
+
+  useEffect(() => {
+    // Set the state with the URL parameters
+    setKeyword(keywordFromUrl);
+    setCategory(categoryFromUrl);
+  }, [keywordFromUrl, categoryFromUrl]);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        The keyword obtained: {keywordFromUrl} <br />
+        The Category from the URL: {categoryFromUrl}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <br />
+      <br />
+      <div>
+        The keyword from state: {keyword} <br />
+        The Category from state: {category}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
